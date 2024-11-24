3a89ad95836ae034f0dcbffbc48675dc8005e5a1
Bono Fox <microfuchs@outlook.com>
2024-11-24 17:43:01 +0100

    🔧 Datenbankzugriff konfiguriert

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: '10.35.47.194',
+        port: 3306,
+        username: await configService.get('DATABASE_USER'),
+        password: await configService.get('DATABASE_PASSWORD'),
+        database: 'k156652_portfolio',
+        synchronize: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
